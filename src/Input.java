81eb36a3a2dfa5cd5c3322376fcf9990888d9103
Dylan Bowman <dbowman06@gmail.com>
2018-11-24 20:01:50 -0800

    Adds Input Class for getting input from the user.

---
+++ src/Input.java
@@ -0,0 +1,46 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Input {
+    private static Scanner scanner;
+
+    private static Scanner getScanner() {
+        if (scanner == null) {
+            scanner = new Scanner(System.in);
+        }
+
+        return scanner;
+    }
+
+    public static String getString(String prompt) {
+        System.out.println(prompt);
+        String out = getScanner().nextLine();
+        return out;
+    }
+
+    static boolean getBoolean(String prompt) {
+        while (true) {
+            String in = getString(prompt);
+            if (in.toLowerCase().equals("y")) {
+                return true;
+            } else if (in.toLowerCase().equals("n")) {
+                return false;
+            } else {
+                System.out.println("That's not y or n!");
+            }
+        }
+    }
+
+    public static int getInt(String prompt) {
+        while (true) {
+            try {
+                System.out.println(prompt);
+                int out = getScanner().nextInt();
+                getScanner().nextLine();
+                return out;
+            } catch (InputMismatchException ex) {
+                System.out.println("That's not an integer.");
+            }
+        }
+    }
+}
